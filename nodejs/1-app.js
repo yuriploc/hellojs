9d6c451ce8b6e39c7115d2bfa459e5143935fba7
Yuri Oliveira <soaresyuri@gmail.com>
2015-11-11 21:39:12 -0300

    trick or treat

--- nodejs/1-app.js
+++ nodejs/1-app.js
@@ -3,4 +3,4 @@ var human = require('./1-human');
 
 hello.hi('aqui no app.js');
 human.myfuncs.itsMe('wario');
-// console.log(module.children[1]);
+// console.log(module.children[1]); // o que isso faz? ;)
