87ef409b09f8869f902edb23f3bb89a170c1da1a
Yuri Oliveira <soaresyuri@gmail.com>
2015-11-24 19:00:37 -0300

    terminando de migrar para Express 4.x

--- nodejs/11-crud-contatos/routes/contatos.js
+++ nodejs/11-crud-contatos/routes/contatos.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
   var contatos = app.controllers.contatos;
   app.get('/contatos', contatos.index);
-  app.get('/contatos/:id', contatos.show);
+  app.get('/contato/:id', contatos.show);
   app.post('/contato', contatos.create);
   app.get('/contato/:id/editar', contatos.edit);
   app.put('/contato/:id', contatos.update);
