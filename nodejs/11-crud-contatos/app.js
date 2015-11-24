52a8fc1f28fefb92f88fa01d8964c159708719d7
Yuri Oliveira <soaresyuri@gmail.com>
2015-11-24 18:57:59 -0300

    terminando de migrar para Express 4.x

--- nodejs/11-crud-contatos/app.js
+++ nodejs/11-crud-contatos/app.js
@@ -1,20 +1,24 @@
 var express = require('express');
-var mySession = require('express-session');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 var load = require('express-load');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var mySession = require('express-session');
+var methodOverride = require('method-override');
 
 var app = express();
 
-// view engine setup
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(cookieParser('keyboard-cat'));
 app.use(mySession({
   secret: 'keyboard-cat',
-  cookie: {}
+  key: 'ntalk.sid',
+  resave:false,
+  saveUninitialized: false,
+  maxAge: 3600000
 }));
 app.use(bodyParser.json()); // bind json + html
-app.use(bodyParser.urlencoded()); // bind json + html em req.body
+app.use(bodyParser.urlencoded({extended: true})); // bind json + html em req.body
 app.use(methodOverride());
 app.use(express.static(__dirname + '/public'));
 
